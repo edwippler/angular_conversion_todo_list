fff52ce3a5d7c5fbaa61657ce7bb8be999dbeabf
Edmund Wippler <ed.wippler@gmail.com>
2017-03-08 14:34:58 -0600

    changed styling

--- public/scripts/controllers/about.controller.js
+++ public/scripts/controllers/about.controller.js
@@ -1,7 +1,7 @@
 myApp.controller('AboutController', function(){
   console.log('AboutController loaded');
   var self = this;
-  self.listOfPeople = [{name:'Ed Wippler', git_username: 'edwippler'},{name: 'Chad Sundburg', git_username: 'chadsundburg'},{name: 'Sam Velie', git_username: 'samvelie'},{name: 'Tom Johander', git_username: 'loPan455'}];
+  self.listOfPeople = [{name:'Ed Wippler', git_username: 'edwippler'},{name: 'Chad Sundberg', git_username: 'chadsundberg'},{name: 'Catherine Jarocki', git_username:'cjaro'}, {name: 'Sam Velie', git_username: 'samvelie'},{name: 'Celina', git_username: 'celina'}, {name: 'Tom Johander', git_username: 'loPan455'}];
 
 
 }); //end of About controller
